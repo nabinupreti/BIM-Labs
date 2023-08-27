d0cc0d8fc5a88a6ba21bcfd257efcf33b13d422e
Nabin Upreti <88663992+nabinupreti@users.noreply.github.com>
2023-08-27 11:52:16 +0545

    Update ApowB.c

--- Second_Semester/Discrete-Structure/Recursive-Problems/ApowB.c
+++ Second_Semester/Discrete-Structure/Recursive-Problems/ApowB.c
@@ -1,21 +1,21 @@
 /*Wap to implement recursive algorithm
--to find a^n*/
+-to find a^b*/
 #include <stdio.h>
 float power(float,int);
 void main(){
 	float a;
-	int n;
+	int b;
 	printf("Enter base: ");
 	scanf("%f",&a);
 	printf("Enter power: ");
-	scanf("%d",&n);
-	printf("%.2f^%d is %.2f.",a,n,power(a,n));
+	scanf("%d",&b);
+	printf("%.2f^%d is %.2f.",a,b,power(a,b));
 }
-float power(float a,int n){
-	if(n==0){
+float power(float a,int b){
+	if(b==0){
 		return 1;
 	}
 	else{
-		return a*power(a,n-1);
+		return a*power(a,b-1);
 	}
 }

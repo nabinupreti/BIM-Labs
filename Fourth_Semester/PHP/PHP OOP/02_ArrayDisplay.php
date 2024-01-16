2af730f64e02cbf11c3eb498c6e34528b9c25d83
Nabin Upreti <88663992+nabinupreti@users.noreply.github.com>
2024-01-16 22:08:07 +0545

    Update 02_ArrayDisplay.php

--- Fourth_Semester/PHP/PHP OOP/02_ArrayDisplay.php
+++ Fourth_Semester/PHP/PHP OOP/02_ArrayDisplay.php
@@ -17,7 +17,7 @@ class ArrayDisplay{
 }
 
 $obj = new ArrayDisplay();
-$friends = array("Hari Parajuli","Nirjal Maharjan","Bikas Bhatta","Rohan Shrestha","Himani Khatri","Sadika Mahat","Pratistha GM","Rasana Maharjan","Ritesh Phuyal","Karuna Chaudhary","Suman Dangol","Apson Kunwar","Sujal Khadgi","Suraj Shakya","Ashim Gurung","Ankit Poudel","Manjesh K. Yadav","Ujan C. Bajracharya","Katrina RL Shah");
+$friends = array("Matina Maharjan","Hari Parajuli","Nirjal Maharjan","Bikas Bhatta","Rohan Shrestha","Himani Khatri","Sadika Mahat","Pratistha GM","Rasana Maharjan","Ritesh Phuyal","Karuna Chaudhary","Suman Dangol","Apson Kunwar","Sujal Khadgi","Suraj Shakya","Ashim Gurung","Ankit Poudel","Manjesh K. Yadav","Ujan C. Bajracharya","Katrina RL Shah");
 $obj->getDetail($friends);
 $obj->displayDetail();
 ?>

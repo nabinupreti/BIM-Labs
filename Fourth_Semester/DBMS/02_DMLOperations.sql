e25c739885b573cd42c3eaf9972bfc929ff922a0
Nabin Upreti <nupreti00@gmail.com>
2024-01-20 22:03:47 +0545

    some files added in dbms

--- Fourth_Semester/DBMS/02_DMLOperations.sql
+++ Fourth_Semester/DBMS/02_DMLOperations.sql
@@ -1,6 +1,34 @@
 /*
 Practical No: - 2
-1)	Create database bank
-2)	Create table transactions having attributes  txnid, acno,type,bank_name
+1) Create database bank
+2) Create table transactions having attributes  txnid, acno,type,
 date-txn ,amount and perform All the  DML operation given above.
 */
+
+CREATE DATABASE bank;
+
+CREATE TABLE transactions(
+	txnid INT PRIMARY KEY,
+	acno BIGINT,
+	actype VARCHAR(10),
+	txn_date DATE,
+	txn_amt FLOAT
+);
+
+/*DML operations*/
+/*INSERT operation*/
+INSERT INTO transactions
+VALUES
+(101,11253621,"Current","2024-01-20",5005.5),
+(102,11253622,"Saving","2024-01-20",25000);
+
+/*SELECT operation*/
+SELECT * FROM transactions;
+
+/*UPDATE operation*/
+UPDATE transactions
+SET txn_amt = 10005.5 WHERE txnid = 101;
+
+/*DELETE operation*/
+DELETE FROM transactions
+WHERE txnid = 102;

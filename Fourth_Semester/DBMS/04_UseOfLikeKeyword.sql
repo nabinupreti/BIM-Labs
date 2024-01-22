697e7c1523e59e127b11d9297bb535cc2965ab52
Nabin Upreti <nupreti00@gmail.com>
2024-01-22 22:36:50 +0545

    use of like keyword

--- Fourth_Semester/DBMS/04_UseOfLikeKeyword.sql
+++ Fourth_Semester/DBMS/04_UseOfLikeKeyword.sql
@@ -5,6 +5,7 @@ ii.	Student address end with ‘r’
 iii.	Student name containing substring ‘ita’
 iv.	Student name end with ‘ta’ and address ‘kathmandu’
 */
+USE dbmslab;
 CREATE TABLE studentData(
 rollno INT PRIMARY KEY,
 sname VARCHAR(30),
@@ -15,7 +16,7 @@ age INT
 INSERT INTO studentData
 (rollno,sname,address,age)
 VALUES
-(101,"Apson","Lalitpur",20),
+(101,"Sabin","Lalitpur",20),
 (102,"Sadika","Bhaktapur",22),
 (103,"Bikas","Rampur",10),
 (104,"Shreeson","Panchkhal",18),
@@ -24,29 +25,30 @@ VALUES
 (107,"Sujal","Pokhara",25),
 (108,"Sabal","Biratnagar",80),
 (109,"Nabin","Bhojpur",22),
-(110,"Nirjal","Baglung",28),
-(111,"Ashika","Birgunj",27),
+(110,"Nikita","Kathmandu",28),
+(111,"Anita","Kathmandu",27),
 (112,"Simran","Bharatpur",24),
-(113,"Roshan","Kavre",20),
+(113,"Rita","Kathmandu",20),
 (114,"Apson","Banepa",21);
 
 SELECT * FROM studentData;
 
+/*Student name start with ‘S’*/
 SELECT sname
 FROM studentdata
 WHERE sname LIKE 'S%';
 
-
+/*Student address end with ‘r’*/
 SELECT address
 FROM studentdata
 WHERE address LIKE '%r';
 
-
+/*Student name containing substring ‘ita’*/
 SELECT sname
 FROM studentdata
-WHERE sname LIKE '%an';
+WHERE sname LIKE '%ita%';
 
-
+/*Student name end with ‘ta’ and address ‘kathmandu’*/
 SELECT sname,address
 FROM studentdata
-WHERE sname LIKE '%n' && address = "lalitpur";
+WHERE sname LIKE '%ta' && address = "Kathmandu";
